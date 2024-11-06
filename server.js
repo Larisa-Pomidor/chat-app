0d805ed9c26e3447d905e3c51c69030401c87f10
Larisa Pomidor <nikakinikakin@gmail.com>
2024-11-06 14:55:45 +0200

    Add initial frontend and backend setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    socket.on('chatMessage', (msg) => {
+        io.emit('chatMessage', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
